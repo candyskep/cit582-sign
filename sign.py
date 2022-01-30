619d1e281290d6a1f82de54c6f77addc8c7c7a38
candyskep <69225515+candyskep@users.noreply.github.com>
2022-01-31 00:10:49 +0800

    Add files via upload

--- sign.py
+++ sign.py
@@ -12,8 +12,10 @@ def sign(m):
 	
 
 	#generate signature
-	r,s = ecdsa.sign(m, private_key, hashfunc=sha256)
 
+	#r,s = ecdsa.sign(m, private_key, hashfunc=sha256)
+	r,s =fastecdsa.ecdsa.sign(msg: m, d: private_key, curve: fastecdsa.curve.Curve = secp256k1, hashfunc=sha256, prehashed: bool = False)
+	
 	assert isinstance( public_key, point.Point )
 	assert isinstance( r, int )
 	assert isinstance( s, int )
